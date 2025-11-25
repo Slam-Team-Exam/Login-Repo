7a946655a844d37ea7f7c53843dfc7530faf45b7
Marc7451 <Marc7451@gmail.com>
2025-11-25 13:46:23 +0100

    new database on pgadmin4

--- Login-System/Login-System/Entities/User.cs
+++ Login-System/Login-System/Entities/User.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-        // beholder Role hvis vi stadig vil lege med Admin senere
+        // Genlav Role hvis vi stadig vil lege med Admin senere
     }
 }
